a00897137f7e48b660c8f46b8eecd80a563ad592
ritikbhatt20 <ritikbhatt020@gmail.com>
2024-09-03 20:03:00 +0530

    nft-staking smart contract

---
+++ programs/nft-staking/src/instructions/mod.rs
@@ -0,0 +1,11 @@
+pub mod claim;
+pub mod initialize_config;
+pub mod initialize_user;
+pub mod stake;
+pub mod unstake;
+
+pub use claim::*;
+pub use initialize_config::*;
+pub use initialize_user::*;
+pub use stake::*;
+pub use unstake::*;
